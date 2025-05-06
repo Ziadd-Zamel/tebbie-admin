6ea6b941b2e17b539e2d7a4cb7d4aa044c1c55dc
hamzamohamed1911 <54458951+hamzamohamed1911@users.noreply.github.com>
2025-05-06 14:45:06 +0300

    update

--- src/pages/RechargeCards.jsx
+++ src/pages/RechargeCards.jsx
@@ -104,7 +104,7 @@ const RechargeCards = () => {
         </Link>
         <button
           onClick={handleExport}
-          className="px-6 py-2 bg-green-500 text-white text-lg rounded-[8px] hover:bg-green-600 flex justify-center items-center gap-2"
+          className="px-6 py-2 border-[#048c87]  text-[#048c87]  text-lg rounded-[8px] hover:bg-[#048c87] hover:text-white transition-all duration-500 flex justify-center items-center gap-2"
         >
           {t("Excel-Export")}
           <FaFileExcel />
