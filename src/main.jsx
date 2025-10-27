22e7f71e9729744f1dfaee2c9b44ead15da03fb6
Ziadd-zamel <ziaddmahmoude@gmail.com>
2025-10-27 09:48:48 +0200

    fix firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import i18n from "./utlis/i18n.js";
 import { AuthProvider } from "./_auth/authContext/JWTProvider.jsx";
 import { ToastContainer } from "react-toastify";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <ToastContainer position="top-right" autoClose={3000} />
-      <App />
+        <App />
     </AuthProvider>
   </React.StrictMode>
 );
